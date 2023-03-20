0119b860716fc0937de09fc452006c1d51d89c06
Maritana Alves <maritana.cz@gmail.com>
2023-03-20 14:25:05 -0300

    Implementação da rota por id de usuário

--- live-01/src/routes/users.route.ts
+++ live-01/src/routes/users.route.ts
@@ -1,9 +1,21 @@
 import { Router, Response, Request, NextFunction } from 'express';
 
+// get /users
+// get /users/:uuid
+// post /users
+// put /users/:uuid
+// delete /users/:uuid
+
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{userName: 'Mari'}];
-  res.status(200).send({users});
+  res.status(200).send(users);
 });
+
+usersRoute.get('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(200).send({uuid});
+})
+
 export default usersRoute;
